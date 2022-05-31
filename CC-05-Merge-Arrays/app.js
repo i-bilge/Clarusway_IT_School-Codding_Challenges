383cf62ec8c5a0dc72d1de8e326a50d00b85f5c6
i-bilge <iismailbilge@gmail.com>
2022-05-31 18:01:42 +0200

    merge arrays challange is studied.

--- CC-05-Merge-Arrays/app.js
+++ CC-05-Merge-Arrays/app.js
@@ -3,4 +3,69 @@ let arrSecond = [6,7,8,9,10];
 
 const mergeArrays = (arr1, arr2) => arr1.concat(arr2);
 
-console.log(mergeArrays(arrFirst, arrSecond));
+console.log("mergeArray is: ", mergeArrays(arrFirst, arrSecond));
+
+console.log(...arrFirst,...arrSecond);
+const mergedArray = [...arrFirst,...arrSecond];
+console.log(mergedArray);
+
+var oneArray = [];
+
+for (let i = 0; i < 11; i++) {
+  
+  oneArray.push(Math.floor(Math.random() * 100));
+  
+}
+var anotherArray = [];
+
+for (let i = 0; i < 11; i++) {
+  
+  anotherArray.push(Math.floor(Math.random() * 100));
+  
+}
+
+
+console.log( Boolean(oneArray));
+console.log( Boolean(anotherArray));
+
+
+function arrayer (arr1, arr2) {
+
+    if (!arr1.length || !arr2.length ) {
+
+        return "One of your arrays is empty";
+
+    } else {
+
+        const arrayed = [...arr1, ...arr2]
+
+    let temp;
+
+    for (let i = 0; i < arrayed.length; i++) {
+
+
+        for (let j = i + 1; j < arrayed.length -1; j++) {
+          
+          
+          if (arrayed[i] > arrayed[j]) {
+            
+            temp = arrayed[i];
+            arrayed[i] = arrayed[j];
+            arrayed[j] = temp;
+            
+          }
+          
+        }
+        
+      }
+
+      return arrayed;
+
+
+    }
+
+    
+
+}
+
+console.log(arrayer(oneArray, anotherArray));
