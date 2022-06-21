1f447295f18b61487ca8e736677335cf1f9cd040
i-bilge <iismailbilge@gmail.com>
2022-06-21 15:14:38 +0200

    CodeChallenge capitalizing letters added.

--- CC-11- CapitalLetters/app.js
+++ CC-11- CapitalLetters/app.js
@@ -2,16 +2,19 @@ let input = document.getElementById("inputArea");
 let button = document.getElementById("convertBtn");
 let output = document.getElementById("outputArea");
 
+//-----------------WAY-1-----------------------
 button.addEventListener("click", () => {
-let splitStr = input.textContent.toLowerCase().split(" ");
-let str;
-
-for (var i = 0; i < splitStr.length; i++) {
-    if (splitStr.length[i] < splitStr.length) {
-      splitStr[i].charAt(0).toUpperCase();
-    }
-
-    str = splitStr.join(" ");
+  const words = input.value.split(" ");
+  for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substring(1);
   }
-  output.textContent = str;
+  output.textContent = words.join(" ");
 });
+
+//-----------------WAY-2-----------------------
+// button.addEventListener("click", () => {
+//   const words = input.value.split(" ");
+//   output.textContent = words.map((word) => { 
+//     return word[0].toUpperCase() + word.substring(1); 
+//   }).join(" ");
+// });
